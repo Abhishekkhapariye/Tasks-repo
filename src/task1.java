b31c8c2f10aceeeca368aea9c477a8d86343630b
abhishek <abhishek.khapariye@ksolves.com>
2022-09-13 15:51:23 +0530

    commited1

---
+++ src/task1.java
@@ -0,0 +1,10 @@
+public class task1 {
+    public static void main(String[] args) {
+        //wrapper class methods
+        Integer i=Integer.valueOf("101011",2);  // wrapper class Integer with it's obj int;
+        int i1=Integer.parseInt("123");
+        int c=i.intValue();
+        System.out.println(i);
+        System.out.println(i1);
+        System.out.println(c);}
+}
